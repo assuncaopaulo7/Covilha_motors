b2eaae46eeedbcb9ab1596200de784caa24db337
joao-mendonca-git <joao-mendonca-2077@outlook.com>
2025-06-02 21:27:40 +0100

    Delete lojaveiculos directory

--- lojaveiculos/src/main/java/pt/ubi/lojaveiculos/repository/InvoiceRepository.java
+++
@@ -1,6 +0,0 @@
-package pt.ubi.lojaveiculos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pt.ubi.lojaveiculos.model.Invoice;
-
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> { }
--- lojaveiculos/src/main/java/pt/ubi/lojaveiculos/repository/SaleRepository.java
+++
@@ -1,6 +0,0 @@
-package pt.ubi.lojaveiculos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pt.ubi.lojaveiculos.model.Sale;
-
-public interface SaleRepository extends JpaRepository<Sale, Long> { }
