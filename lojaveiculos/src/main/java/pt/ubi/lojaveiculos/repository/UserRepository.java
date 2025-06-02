98377ed676004a2f521f128cd3e2a1fa59adc226
joao-mendonca-git <joao-mendonca-2077@outlook.com>
2025-06-02 12:14:53 +0100

    Delete lojaveiculos directory

--- lojaveiculos/src/main/java/pt/ubi/lojaveiculos/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package pt.ubi.lojaveiculos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pt.ubi.lojaveiculos.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
